2abbbf15074b3c55b5a5f3d1065f77f6cad3d746
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-07-03 10:40:44 -0700

    turn MPCORB2MonthlyCatalog.py into a function

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from astropy.time import Time
 import pickle
 import time
 
@@ -16,18 +16,75 @@ import tensorflow as tf
 from tensorflow.keras import models
 
 
+def convert_mpcorb_to_monthly_catalog(filename_in, filename_out):
+    raw_catalog_list = []
+    f = open(filename_in, "r")
+    for line in f:
+        if line == "\n":
+            continue  # in case there's a blank
+        else:  # line in the original data file
+            raw_catalog_list.append(line.rstrip())
+    f.close()
+
+    for n in range(100):
+        if raw_catalog_list[n] == '-' * 160:
+            start_line = n + 1
+            # to define the start of the actual data table,
+            # which comes after ~30 lines of header text
+
+    cropped_catalog_list = []
+    # crop off the header
+    for n in range(len(raw_catalog_list) - start_line):
+        cropped_catalog_list.append(raw_catalog_list[n + start_line])
+
+    full_catalog = []
+
+    for obj_mpc in cropped_catalog_list:
+        abs_m_H = obj_mpc[8:14].strip()
+        slope_G = obj_mpc[14:20].strip()
+        epoch = obj_mpc[20:26].strip()
+        mean_anomaly_M = obj_mpc[26:36].strip()
+        peri = obj_mpc[37:47].strip()
+        node = obj_mpc[48:58].strip()
+        inclin = obj_mpc[59:69].strip()
+        eccen = obj_mpc[70:80].strip()
+        motion_n = obj_mpc[80:92].strip()
+        a = obj_mpc[92:104].strip()
+        unc_U = obj_mpc[105:107].strip()
+        readable_designation = obj_mpc[166:194].strip()
+
+        # MPC format has a "packed" date, allowing the epoch to be stored in
+        # fewer digits. However, this must be converted to mm/dd/yyyy format
+        # for XEphem.
+        epoch_x = f'{int(epoch[3], 36):02d}/{int(epoch[4], 36):02d}.0/{int(epoch[0], 36):02d}{epoch[1:3]}'
+
+        if unc_U == "":
+            unc_U = "?"
+        expanded_designation = readable_designation + " " + unc_U
+
+        # Write XEphem format orbit to the full_catalog list.
+        full_catalog.append(expanded_designation + ",e," + inclin + ","
+                            + node + "," + peri + "," + a + "," + motion_n + "," + eccen + "," +
+                            mean_anomaly_M + "," + epoch_x + "," + "2000" + ",H " + abs_m_H +
+                            "," + slope_G + "\n")
+
+    f2 = open(filename_out, "w")
+    for obj in full_catalog:
+        f2.write(obj)
+    f2.close()
+
+
 def movingobjectcatalog(obsmjd):
     catalog_list = []
-    t0 = datetime.datetime(1858, 11, 17)
-    tobs = t0 + datetime.timedelta(obsmjd)
-    fnam = f"{tobs.year:04d}_{tobs.month:02d}_ORB.DAT"
+    tobs = Time(obsmjd, format='mjd').strftime('%Y_%m')
+    fnam = f"{tobs}_ORB.DAT"
     if not os.path.exists(fnam):
         os.system('wget -O MPCORB.DAT http://www.minorplanetcenter.org/iau/MPCORB/MPCORB.DAT')
-        os.system('python MPCORB2MonthlyCatalog.py')
+        convert_mpcorb_to_monthly_catalog('MPCORB.DAT', fnam)
     elif time.time() - os.path.getmtime(fnam)>(24*60*60):
-        os.system('rm '+fnam)
+        os.remove(fnam)
         os.system('wget -O MPCORB.DAT http://www.minorplanetcenter.org/iau/MPCORB/MPCORB.DAT')
-        os.system('python MPCORB2MonthlyCatalog.py')
+        convert_mpcorb_to_monthly_catalog('MPCORB.DAT', fnam)
     with open(fnam) as f_catalog:
         for line in f_catalog:
             catalog_list.append(line.rstrip())
@@ -38,8 +95,7 @@ def movingobjectcatalog(obsmjd):
 def movingobjectfilter(s_catalog, s_ra, s_dec, obsmjd, filter_radius):
     DEG2RAD = 0.01745329252
     s_ra_radians, s_dec_radians = s_ra * DEG2RAD, s_dec * DEG2RAD
-    t0 = datetime.datetime(1858, 11, 17)
-    tobs = t0 + datetime.timedelta(obsmjd)
+    tobs = Time(obsmjd, format='mjd')
 
     s_full_date = tobs.strftime("%Y/%m/%d %H:%M:%S")
     s_date = ephem.date(s_full_date)
