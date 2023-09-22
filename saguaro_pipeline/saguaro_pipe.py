2317e32801f5ec167b082c1e3a7ce8fb58272bf9
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-09-21 20:52:06 -0400

    increment __version__

--- saguaro_pipeline/saguaro_pipe.py
+++ saguaro_pipeline/saguaro_pipe.py
@@ -4,7 +4,7 @@
 Pipeline for real-time data reduction and image subtraction.
 """
 
-__version__ = "2.0.1"  # last updated 2023-09-07
+__version__ = "2.0.2"  # last updated 2023-09-21
 
 import argparse
 import datetime
