0d83badc55898fc92c666124e630fc02b54307f9
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2024-04-23 09:58:11 -0400

    use real limiting magnitude when ingesting preplanned obs

--- saguaro_pipeline/css.py
+++ saguaro_pipeline/css.py
@@ -4,7 +4,7 @@
 Telescope setting file for CSS 1.5m Mt Lemmon telescope.
 """
 
-__version__ = "2.1.6"  # last updated 2024-04-23
+__version__ = "2.1.7"  # last updated 2024-04-23
 
 import datetime
 import gc
